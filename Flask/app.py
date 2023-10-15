ce6945e8149770096d164f73933ef6ccb8d488d5
Liliya Atanasova <valvalatan04@gmail.com>
2023-10-15 10:49:48 -0400

    sum

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
@@ -9,6 +9,15 @@ def index():
 @app.route('/about')
 def about():
     return render_template('Page2.html')
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    if request.method == 'POST':
+        ip_list = request.form.getlist('ip_address')
+        # Process the list of IP addresses as needed
+        print(ip_list)  # Example: Printing the list of IP addresses
+        return 'IP addresses received successfully'
+
 
 
 if __name__ == "__main__":
