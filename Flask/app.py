d24e1c46f8fa78a024fb60c81bb53e30ea489abf
zehayk <hayk.minecraft@gmail.com>
2023-10-15 15:36:45 -0400

    sum shit

--- Flask/app.py
+++ Flask/app.py
@@ -9,7 +9,7 @@ added_ips = []
 def index():
     return render_template('Page1.html', added_ips=added_ips)
 
-@app.route('/about')
+@app.route('/Page2')
 def about():
     return render_template('Page2.html')
 
@@ -21,9 +21,16 @@ def add_ip():
         update_node_file()
     return index()
 
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    new_ip = request.form.get('messageInput')
+    if new_ip and new_ip.strip():
+        added_ips.append(new_ip)
+    return index()
+
 def update_node_file():
     try:
-        with open('list_of_nodes.txt', 'w') as f:
+        with open('list_of_nodes.txt', 'w+') as f:
             ip_string = ', '.join(added_ips)
             f.write(ip_string)
     except Exception as e:
